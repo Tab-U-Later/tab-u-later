59c09cabad772eea24b7d32f3425498502dd90af
Ben Reitman <ben.reitman@gmail.com>
2020-11-19 18:26:24 -0800

    (fix) fixed functionality for valid edits

--- src/client/components/cards/EditCard.js
+++ src/client/components/cards/EditCard.js
@@ -36,6 +36,12 @@ const DoneButton = styled(IconButton)`
   margin-right: 5px !important;
 `
 
+const ListContainer = styled(ListItem)`
+  display: flex;
+  flex-direction: row;
+  justify-content space-between;
+`
+
 
 const EditCard = (props) => {
   const [newName, updateSeshName] = useState(null)
@@ -46,10 +52,10 @@ const EditCard = (props) => {
   const [isDone, setDone] = useState(false);
   const [isValid, setValidName] = useState(false);
 
-  useEffect( () => {
+  useEffect(() => {
     let selects = {}
     tabs.forEach((tab) => {
-      selects = {...selects, [tab.title]: { checked: false, url: tab.url }}
+      selects = { ...selects, [tab.title]: { checked: false, url: tab.url } }
     })
     setSelections(selects)
   }, [])
@@ -66,9 +72,9 @@ const EditCard = (props) => {
 
   const removeTabs = () => {
     let newSesh = [];
-    Object.entries(selections).forEach(([key,value]) => {
-      if(value.checked === false){
-        newSesh.push({url: value.url, title: key})
+    Object.entries(selections).forEach(([key, value]) => {
+      if (value.checked === false) {
+        newSesh.push({ url: value.url, title: key })
       }
     })
     setTabs(newSesh);
@@ -79,20 +85,20 @@ const EditCard = (props) => {
   }
 
   const handleChange = (event, url) => {
-    setSelections({...selections, [event.target.name]: { checked: event.target.checked, url: url } })
+    setSelections({ ...selections, [event.target.name]: { checked: event.target.checked, url: url } })
   }
-/*TODO: check errors for URL and display proper error warning*/
+  /*TODO: check errors for URL and display proper error warning*/
   const addTab = () => {
     fetchTitle(addUrl, (content) => {
-      chrome.storage.sync.set({[props.name]: [...tabs, content]})
+      chrome.storage.sync.set({ [props.name]: [...tabs, content] })
       setTabs([...tabs, content])
     })
   }
 
   useEffect(() => {
     let anyChecked = false;
-    for(const key in selections){
-      if(selections[key]['checked'] === true){
+    for (const key in selections) {
+      if (selections[key]['checked'] === true) {
         anyChecked = true;
       }
     }
@@ -114,6 +120,9 @@ const EditCard = (props) => {
         <List subheader={<ListSubheader disableSticky>Change Session Name</ListSubheader>}>
           <ListItem>
             <TField id="outlined-basic" label="Rename Session" variant="outlined" size="small" value={newName} onChange={(e) => updateSeshName(e.target.value)} />
+            <IconButton color={(newName === null || newName.length < 0) ? 'secondary': 'default'} disabled={newName === null || newName.length < 0}onClick={updateName}>
+              <Check />
+            </IconButton>
           </ListItem>
         </List>
 
@@ -122,36 +131,24 @@ const EditCard = (props) => {
         <List subheader={<ListSubheader disableSticky>Add A Tab</ListSubheader>}>
           <ListItem>
             <TField id="outlined-basic" label="Tab URL" variant="outlined" size="small" value={addUrl} onChange={(e) => updateUrl(e.target.value)} />
+            <IconButton onClick={() => addTab()}>
+              <Check />
+            </IconButton>
           </ListItem>
-          <IconButton onClick={() => addTab()}>
-            <Check/>
-          </IconButton>
         </List>
 
         <Divider />
 
-        <ButtonContainer>
-          {isDone ?
-          <IconButton color='secondary'>
-            <Remove onClick={removeTabs}/>
-          </IconButton>: null}
-          {isValid ?
-          <DoneButton color='secondary' onClick={updateName}>
-            <Done/>
-          </DoneButton> : null}
-        </ButtonContainer>
-
-        <Divider />
         <List subheader={<ListSubheader disableSticky>Saved Tabs</ListSubheader>}>
           {tabs.map((tab) => (
             <ListItem>
               <FormControlLabel
                 control={<Checkbox color='primary'
-                checked={selections[tab.title] ? selections[tab.title]['checked'] : false}
-                onChange={(e) => handleChange(e, tab.url)}
-                name={tab.title} />}
+                  checked={selections[tab.title] ? selections[tab.title]['checked'] : false}
+                  onChange={(e) => handleChange(e, tab.url)}
+                  name={tab.title} />}
                 label={tab.title.length > 15 ? tab.title.substring(0, 15) + '...' : tab.title}
-                checkedIcon={<DeleteForever/>}
+                checkedIcon={<DeleteForever />}
               />
             </ListItem>
           ))}
